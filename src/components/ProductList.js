f90fcf6a44f9c37ad4c06e97f72102a4dbc95ada
StuartCowley <voidstalker@live.co.uk>
2022-09-27 20:29:40 +0100

    fill out product list component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/product-list.css";
 
 const ProductList = ({ products }) => {
   return (
-    <div>
+    <div className="product-list__wrap">
       {products.map((product) => {
-        return <p>{product.title}</p>;
+        return (
+          <div key={product.id} className="product-list__item-wrap">
+            <div className="product-list__item-header">{product.title}</div>
+            <div className="product-list__item-body">
+              <div className="product-list__column">
+                <div>Product Rating: {product.rating}</div>
+                <div>Product type: {product.type}</div>
+              </div>
+              <div className="product-list__column">
+                <div>Description: {product.description}</div>
+              </div>
+            </div>
+            <Link
+              className="product-list__item-link"
+              to={`product/${product.id}`}
+            >
+              Details
+            </Link>
+          </div>
+        );
       })}
     </div>
   );
