f409e920f7f57b22afeaf8f91c34e5ae87089149
StuartCowley <voidstalker@live.co.uk>
2022-09-27 19:54:49 +0100

    add routes and install react router dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
-import '../styles/app.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import "../styles/app.css";
 
 const App = () => {
   return (
     <div className="app">
-    <h1>Example shop</h1>
-    <nav>
-      <ul className='app__nav-list'>
-        <li className='app__nav-list-item'>Homepage</li>
-        <li className='app__nav-list-item'>Products</li>
-        <li className='app__nav-list-item'>About</li>
-        <li className='app__nav-list-item'>Contact</li>
-      </ul>
-    </nav>
+      <Router>
+        <h1>Example shop</h1>
+        <nav>
+          <ul className="app__nav-list">
+            <li className="app__nav-list-item">Homepage</li>
+            <li className="app__nav-list-item">Products</li>
+            <li className="app__nav-list-item">About</li>
+            <li className="app__nav-list-item">Contact</li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<h2>Home component</h2>} />
+          <Route path="products" element={<h2>Products component</h2>} />
+          <Route path="about" element={<h2>About component</h2>} />
+          <Route path="contact" element={<h2>Contact component</h2>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
